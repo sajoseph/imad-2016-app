aee8874ebe2c80cb01cfd4fc2d1e6c004240bc13
sajoseph <samuel.a.pio@gmail.com>
2016-10-16 13:14:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-1', function (req, res) {
-  res.send("This is article1");
+ res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
 });
 
 app.get('/article-2', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', '/article-2.html'));
 });
 
 app.get('/article-3', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', '/article-3.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
